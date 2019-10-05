3060485c5b8f915b8aa265d3f34d72868a9502a0
unknown <kkorenkiewicz1@gmai.com>
2019-10-05 16:13:52 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { RoomsComponent } from './accomodation/rooms/rooms.component';
+import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
+import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-beverage/bars/bars.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WellnesAndSpaComponent } from './lifestyle/wellnes-and-spa/wellnes-and-spa.component';
+import { ResortAndActivitiesComponent } from './lifestyle/resort-and-activities/resort-and-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { WesternWeddingsComponent } from './weddings/western-weddings/western-weddings.component';
+import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { OffersComponent } from './offers/offers.component';
+import { AuthComponent } from './auth/auth.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccomodationComponent,
+    RoomsComponent,
+    FoodBeverageComponent,
+    RestaurantComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WellnesAndSpaComponent,
+    ResortAndActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    WesternWeddingsComponent,
+    ThaiWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
